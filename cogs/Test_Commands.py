82a9514923fb54107d5f7887fe0f514807430dbf
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-14 00:51:24 +0100

    Update Test_Commands.py

--- cogs/Test_Commands.py
+++ cogs/Test_Commands.py
@@ -11,17 +11,17 @@ class Test_Commands(commands.Cog):
         self.client = client
 
 
-@commands.Cog.listener()
-async def on_ready(self):
-    print ('Test Command File Loaded')
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print ('Test Command File Loaded')
 
-@commands.command()
-async def gilbert(self, ctx):
-    gilbertN = 0
-    while gilbertN < 3:
-        await ctx.send('https://imgur.com/8DjzVc5')
-        gilbertN += 1
-        print ({gilbertN})
+    @commands.command()
+    async def gilbert(self, ctx):
+        gilbertN = 0
+        while gilbertN < 3:
+            await ctx.send('https://imgur.com/8DjzVc5')
+            gilbertN += 1
+            print ({gilbertN})
 
 def setup(client):
     client.add_cog(Test_Commands(client))
