dcc277f24d1c68cf669340795e5b79de22ab329f
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-13 01:53:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,20 @@ async def change_status():
         await client.change_presence(activity=discord.Game(name=current_status))
         await asyncio.sleep(60)
         print ('Status Changed')
-           
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Missing required argument')
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Command does not exist')
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send('You do not have permission to use this command')        
+        
+@client.command()
+async def Commands(ctx):
+    await ctx.send('__**Commands: **__ ' + '```(x = Amount Of Pings)\nSpamVoss(x)\nSpamBaldwin(x)\nSpamJay(x)\nSpamSimon(X)\nSpamConnor(x)\nSpamAaron(x)\nSpamNiall(x)\nSpamPete(x)```')
+            
 @client.command()
 async def Test(ctx):
     await ctx.send('working')
