daaf398b8950671100937ec52fd9ce8beafdef43
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-13 20:32:38 +0100

    added auto gif response

--- bot.py
+++ bot.py
@@ -53,6 +53,14 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CheckFailure):
         await ctx.send('You do not have permission to use this command')
 
+@client.event
+async def on_message(message):
+    member = message.author.id
+    if message.author.id == 151063490168094721:
+        if "gif" in message.content:
+            responses = ['https://imgur.com/SFY00cc','https://imgur.com/l1isdSZ','https://imgur.com/XiST5sG','https://imgur.com/Q0c3E8c','https://imgur.com/YFwZgbl','https://imgur.com/vlkOGaf','https://imgur.com/aWyR7iX', 'https://imgur.com/8DjzVc5', 'https://imgur.com/ovOGXf8','https://imgur.com/8HMehnO','https://imgur.com/jvlfhzn','https://imgur.com/8KMJ6sr']
+            await message.channel.send(random.choice(responses))
+
 @client.command()
 async def Commands(ctx):
     await ctx.send('__**Commands: **__ ' + '```(x = Amount Of Pings)\nSpamVoss(x)\nSpamBaldwin(x)\nSpamJay(x)\nSpamSimon(X)\nSpamConnor(x)\nSpamAaron(x)\nSpamNiall(x)\nSpamPete(x)```')
