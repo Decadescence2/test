100a4a406334abcc1796ef4427289c9994ba37ef
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-13 01:38:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,14 +27,14 @@ Messages = ['With My 2 Balls', 'With Baldwins Sister', '7 Days Until This Game D
 #     print('Bot is Online, Loading Files...')
 
 
-# @client.event
-# async def on_message(message):
-#     member = message.author.id
+@client.event
+async def on_message(message):
+    member = message.author.id
 
-#     if message.author.id == 151063490168094721:
-#         if "gif" in message.content:
-#             responses = ['https://imgur.com/SFY00cc','https://imgur.com/l1isdSZ','https://imgur.com/XiST5sG','https://imgur.com/Q0c3E8c','https://imgur.com/YFwZgbl','https://imgur.com/vlkOGaf','https://imgur.com/aWyR7iX', 'https://imgur.com/8DjzVc5', 'https://imgur.com/ovOGXf8','https://imgur.com/8HMehnO','https://imgur.com/jvlfhzn','https://imgur.com/8KMJ6sr']
-#             await message.channel.send(random.choice(responses))
+    if message.author.id == 151063490168094721:
+        if "gif" in message.content:
+            responses = ['https://imgur.com/SFY00cc','https://imgur.com/l1isdSZ','https://imgur.com/XiST5sG','https://imgur.com/Q0c3E8c','https://imgur.com/YFwZgbl','https://imgur.com/vlkOGaf','https://imgur.com/aWyR7iX', 'https://imgur.com/8DjzVc5', 'https://imgur.com/ovOGXf8','https://imgur.com/8HMehnO','https://imgur.com/jvlfhzn','https://imgur.com/8KMJ6sr']
+            await message.channel.send(random.choice(responses))
 
 # @client.command()
 # async def Commands(ctx):
@@ -45,5 +45,5 @@ Messages = ['With My 2 Balls', 'With Baldwins Sister', '7 Days Until This Game D
 async def Test(ctx):
     await ctx.send('working')
 
-
+client.loop.create_task(change_status())
 client.run(os.environ['token'])
