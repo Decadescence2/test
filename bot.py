bbf4da55ec142a652ae421e207d9a110c27f5fb9
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-13 01:49:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import random
 import time
 import os
 import asyncio
-# import aiohttp
 
 from discord.ext import commands, tasks
 from itertools import cycle
@@ -11,14 +10,7 @@ from itertools import cycle
 
 client = commands.Bot(command_prefix = '=')
 
-##################################################################
-
-Messages = ['With My 2 Balls', 'With Baldwins Sister', '7 Days Until This Game Dies',
-            'With My Amazing Headset', 'UNO.', 'With My Fallout Strap On Mod', 'Speaking Over Niall'
-            , 'Dont Starve IRL', '~']
-
-##################################################################
-
+Messages = ['1', '2', '3']
 
 
 # #Events
@@ -33,22 +25,9 @@ async def change_status():
     while not client.is_closed():
         current_status = next(status)
         await client.change_presence(activity=discord.Game(name=current_status))
-        await asyncio.sleep(5)
+        await asyncio.sleep(60)
         print ('Status Changed')
-            
-@client.event
-async def on_message(message):
-    member = message.author.id
-
-    if message.author.id == 151063490168094721:
-        if "gif" in message.content:
-            responses = ['https://imgur.com/SFY00cc','https://imgur.com/l1isdSZ','https://imgur.com/XiST5sG','https://imgur.com/Q0c3E8c','https://imgur.com/YFwZgbl','https://imgur.com/vlkOGaf','https://imgur.com/aWyR7iX', 'https://imgur.com/8DjzVc5', 'https://imgur.com/ovOGXf8','https://imgur.com/8HMehnO','https://imgur.com/jvlfhzn','https://imgur.com/8KMJ6sr']
-            await message.channel.send(random.choice(responses))
-
-# @client.command()
-# async def Commands(ctx):
-#     await ctx.send('__**Commands: **__ ' + '```(x = Amount Of Pings)\nSpamVoss(x)\nSpamBaldwin(x)\nSpamJay(x)\nSpamSimon(X)\nSpamConnor(x)\nSpamAaron(x)\nSpamNiall(x)\nSpamPete(x)```')
-
+           
 @client.command()
 # @commands.check(IDCheck)
 async def Test(ctx):
