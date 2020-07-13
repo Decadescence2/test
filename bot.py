f9543f1d4325868b2ac0a31dc8834468140fe5d8
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-13 01:27:55 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -11,6 +11,40 @@ from itertools import cycle
 
 client = commands.Bot(command_prefix = '=')
 
+##################################################################
+
+Messages = ['With My 2 Balls', 'With Baldwins Sister', '7 Days Until This Game Dies',
+            'With My Amazing Headset', 'UNO.', 'With My Fallout Strap On Mod', 'Speaking Over Niall'
+            , 'Dont Starve IRL', '~']
+
+##################################################################
+
+
+
+#Events
+@client.event
+async def on_ready():
+    print('Bot is Online, Loading Files...')
+
+
+@client.event
+async def on_message(message):
+    member = message.author.id
+
+    if message.author.id == 151063490168094721:
+        if "gif" in message.content:
+            responses = ['https://imgur.com/SFY00cc','https://imgur.com/l1isdSZ','https://imgur.com/XiST5sG','https://imgur.com/Q0c3E8c','https://imgur.com/YFwZgbl','https://imgur.com/vlkOGaf','https://imgur.com/aWyR7iX', 'https://imgur.com/8DjzVc5', 'https://imgur.com/ovOGXf8','https://imgur.com/8HMehnO','https://imgur.com/jvlfhzn','https://imgur.com/8KMJ6sr']
+            await message.channel.send(random.choice(responses))
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Missing required argument')
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Command does not exist')
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send('You do not have permission to use this command')
+
 @client.command()
 # @commands.check(IDCheck)
 async def Test(ctx):
