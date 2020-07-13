c8dd54672ceb43d330da7258b1dd067b7fb79619
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-13 01:06:54 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -9,8 +9,6 @@ from discord.ext import commands, tasks
 from itertools import cycle
 
 
-TOKEN = 'NzMyMDIwMzMxMzE2NzcyOTk2.XwuhGQ.4xRE95CJXLcVwd4dQvnyAgHZg_I'
-
 client = commands.Bot(command_prefix = '=')
 
 @client.command()
@@ -19,4 +17,4 @@ async def Test(ctx):
     await ctx.send('working')
 
 
-client.run(TOKEN)
+client.run(os.environ['token'])
