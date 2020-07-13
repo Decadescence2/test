f3fc42d20944705fb9b6b65e766b727d5a4b3990
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-13 01:36:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,24 +21,24 @@ Messages = ['With My 2 Balls', 'With Baldwins Sister', '7 Days Until This Game D
 
 
 
-#Events
-@client.event
-async def on_ready():
-    print('Bot is Online, Loading Files...')
+# #Events
+# @client.event
+# async def on_ready():
+#     print('Bot is Online, Loading Files...')
 
 
-@client.event
-async def on_message(message):
-    member = message.author.id
+# @client.event
+# async def on_message(message):
+#     member = message.author.id
 
-    if message.author.id == 151063490168094721:
-        if "gif" in message.content:
-            responses = ['https://imgur.com/SFY00cc','https://imgur.com/l1isdSZ','https://imgur.com/XiST5sG','https://imgur.com/Q0c3E8c','https://imgur.com/YFwZgbl','https://imgur.com/vlkOGaf','https://imgur.com/aWyR7iX', 'https://imgur.com/8DjzVc5', 'https://imgur.com/ovOGXf8','https://imgur.com/8HMehnO','https://imgur.com/jvlfhzn','https://imgur.com/8KMJ6sr']
-            await message.channel.send(random.choice(responses))
+#     if message.author.id == 151063490168094721:
+#         if "gif" in message.content:
+#             responses = ['https://imgur.com/SFY00cc','https://imgur.com/l1isdSZ','https://imgur.com/XiST5sG','https://imgur.com/Q0c3E8c','https://imgur.com/YFwZgbl','https://imgur.com/vlkOGaf','https://imgur.com/aWyR7iX', 'https://imgur.com/8DjzVc5', 'https://imgur.com/ovOGXf8','https://imgur.com/8HMehnO','https://imgur.com/jvlfhzn','https://imgur.com/8KMJ6sr']
+#             await message.channel.send(random.choice(responses))
 
-@client.command()
-async def Commands(ctx):
-    await ctx.send('__**Commands: **__ ' + '```(x = Amount Of Pings)\nSpamVoss(x)\nSpamBaldwin(x)\nSpamJay(x)\nSpamSimon(X)\nSpamConnor(x)\nSpamAaron(x)\nSpamNiall(x)\nSpamPete(x)```')
+# @client.command()
+# async def Commands(ctx):
+#     await ctx.send('__**Commands: **__ ' + '```(x = Amount Of Pings)\nSpamVoss(x)\nSpamBaldwin(x)\nSpamJay(x)\nSpamSimon(X)\nSpamConnor(x)\nSpamAaron(x)\nSpamNiall(x)\nSpamPete(x)```')
 
 @client.command()
 # @commands.check(IDCheck)
