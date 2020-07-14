f125e623ac2ff20ad85a6ae3adf640bd773bf683
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-14 01:25:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,7 @@ from itertools import cycle
 
 client = commands.Bot(command_prefix = '~')
 
-Messages = ['With My 2 Balls', 'With Baldwins Sister', '7 Days Until This Game Dies',
-            'With My Amazing Headset', 'UNO.', 'With My Fallout Strap On Mod', 'Speaking Over Niall'
-            , 'Dont Starve IRL', '~']
+Messages = ['1', '2', '3','4']
 
 @client.command()
 async def Load(ctx, extension):
@@ -33,7 +31,7 @@ for filename in os.listdir('./cogs'):
 
 @client.event
 async def on_ready():
-    print('Bot is Online, Loading Files...')
+    print('Bot is Online')
 
 async def change_status():
     await client.wait_until_ready()
@@ -57,18 +55,18 @@ async def on_command_error(ctx, error):
 @client.event
 async def on_message(message):
     member = message.author.id
-    if message.author.id == 339508544409829376:
+    if message.author.id == XXXXX:
         if "gif" in message.content:
-            responses = ['https://imgur.com/SFY00cc','https://imgur.com/l1isdSZ','https://imgur.com/XiST5sG','https://imgur.com/Q0c3E8c','https://imgur.com/YFwZgbl','https://imgur.com/vlkOGaf','https://imgur.com/aWyR7iX', 'https://imgur.com/8DjzVc5', 'https://imgur.com/ovOGXf8','https://imgur.com/8HMehnO','https://imgur.com/jvlfhzn','https://imgur.com/8KMJ6sr']
+            responses = ['This part works','']
             await message.channel.send(random.choice(responses))
 
 @client.command()
 async def Commands(ctx):
-    await ctx.send('__**Commands: **__ ' + '```(x = Amount Of Pings)\nSpamVoss(x)\nSpamBaldwin(x)\nSpamJay(x)\nSpamSimon(X)\nSpamConnor(x)\nSpamAaron(x)\nSpamNiall(x)\nSpamPete(x)```')
+    await ctx.send('shows commands')
 
 @client.command()
 async def Test(ctx):
-    await ctx.send('working')
+    await ctx.send('command works')
 
 client.loop.create_task(change_status())
 client.run(os.environ['token'])
