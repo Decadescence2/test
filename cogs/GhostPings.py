5b041f72e97421bd2ca91247f842a45e63fcb472
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-22 23:31:01 +0100

    Add files via upload

--- cogs/GhostPings.py
+++ cogs/GhostPings.py
@@ -4,6 +4,16 @@ import os
 from itertools import cycle
 from discord.ext import commands
 
+MyID3 = '<@339508544409829376>'
+VossID3 = '<@215985007326527488>'
+BaldwinID3 = '<@248950488731484161>'
+JayID3 = '<@380887572970340362>'
+ConnorID3  = '<@151063490168094721>'
+AaronID3 = '<@176810214257852416>'
+NiallID3 = '<@246568122457391105>'
+PeteID3 = '<@400336685084311552>'
+SimonID3 = '<@374657763269410836>'
+
 class GhostPings(commands.Cog):
 
     def __init__(self, client):
@@ -14,8 +24,8 @@ class GhostPings(commands.Cog):
         print ('GhostPing file Loaded')
 
     @commands.command()
-    async def GhostTest(self, ctx, *, question):
-        await ctx.send('**LURKING!?** ' + MyID, delete_after=2);
+    async def GhostTest(self, ctx):
+        await ctx.send(MyID3, delete_after=2);
         await ctx.message.delete()
 
 def setup(client):
