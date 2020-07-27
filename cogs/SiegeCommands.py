0fb78cc094beeabe5b1af47cd6d062cd6f2b7df3
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-07-27 01:58:21 +0100

    Add files via upload

--- cogs/SiegeCommands.py
+++ cogs/SiegeCommands.py
@@ -10,17 +10,16 @@ class SiegeCommands(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    maps = ['Bank','Bartlett University','Chalet','Clubhouse','Consulate','Hereford Base',
-    'House','Kafe Dostoyevsky','Kanal','Oregon','Outback','Presidential Plane',
-    'Yacht','Border','Favela','Fortress','Skyscraper','Coastline','Villa','Theme Park']
-
     @commands.Cog.listener()
     async def on_ready(self):
         print ('Siege File Loaded')
 
     @commands.command()
     async def RandomMap(self, ctx):
-        await message.channel.send(random.choice(maps))
+        maps = ['Bank','Bartlett University','Chalet','Clubhouse','Consulate','Hereford Base',
+        'House','Kafe Dostoyevsky','Kanal','Oregon','Outback','Presidential Plane',
+        'Yacht','Border','Favela','Fortress','Skyscraper','Coastline','Villa','Theme Park']
+        await ctx.send(random.choice(maps))
 
 
 def setup(client):
