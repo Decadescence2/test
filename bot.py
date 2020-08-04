9a4326accbd04eac74b0c9c78fd0c9c0910c2ee3
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-08-04 17:45:38 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ import random
 
 from discord.ext import commands, tasks
 from itertools import cycle
+from donuts import *
+
+target_channel_id = 523703758564360197
 
 client = commands.Bot(command_prefix = '~', case_insensitive=True)
 client.remove_command('help')
@@ -76,6 +79,32 @@ async def on_message(message):
             responses = ['https://imgur.com/SnGm07p', 'https://imgur.com/xUcha8d''https://imgur.com/SFY00cc','https://imgur.com/l1isdSZ','https://imgur.com/XiST5sG','https://imgur.com/Q0c3E8c','https://imgur.com/YFwZgbl','https://imgur.com/vlkOGaf','https://imgur.com/aWyR7iX', 'https://imgur.com/8DjzVc5', 'https://imgur.com/ovOGXf8','https://imgur.com/8HMehnO','https://imgur.com/jvlfhzn','https://imgur.com/8KMJ6sr']
             await message.channel.send(random.choice(responses))
 
+@tasks.loop(seconds=30)
+async def called_once_a_week():
+    message_channel = client.get_channel(target_channel_id)
+    print(f"Got channel {message_channel}")
+    await message_channel.send("reset")
+    global Niall
+    global Voss
+    global Connor
+    global Baldwin
+    global Ash
+    global Pete
+    global Jay
+    global Aaron
+    Niall = 0
+    Voss = 0
+    Connor = 0
+    Baldwin = 0
+    Jay = 0
+    Ash = 0
+    Pete = 0
+    Aaron = 0
+
+@called_once_a_week.before_loop
+async def before():
+    await client.wait_until_ready()
+    print("Finished waiting")
 
 @client.command()
 async def Test(ctx):
