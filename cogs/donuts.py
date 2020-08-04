7e029be2a425db584f0d74bb9348bbf77feb0919
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-08-04 02:26:50 +0100

    Add files via upload

--- cogs/donuts.py
+++ cogs/donuts.py
@@ -26,13 +26,13 @@ class donuts(commands.Cog):
 
     @commands.command(pass_context = True)
     async def AddDonutVoss(self, ctx):
-        Voss += 1
+        Voss + 1
         await ctx.send('https://imgur.com/Z7T1Gh5')
 
     @commands.command(pass_context = True)
     async def Leaderboard(self, ctx):
         await ctx.send('Baldwin: ');
-        await ctx.send('Baldwin: ' + Voss);
+        await ctx.send(f'Baldwin: ' + Voss);
         # await ctx.send('Voss: ' + Voss);
         # await ctx.send('Ash: ' + Ash);
         # await ctx.send('Jay: ' + Jay);
