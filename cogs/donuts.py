e97495a1be8553128239a5f07e47b156637c9030
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-08-04 02:13:05 +0100

    Add files via upload

--- cogs/donuts.py
+++ cogs/donuts.py
@@ -5,14 +5,14 @@ from itertools import cycle
 from discord.ext import commands
 
 #donuts
-Baldwin = int(0)
-Ash = int(0)
-Voss = int(0)
-Jay = int(0)
-Connor = int(0)
-Aaron = int(0)
-Pete = int(0)
-Niall = int(0)
+Baldwin = 0
+Ash = 0
+Voss = 0
+Jay = 0
+Connor = 0
+Aaron = 0
+Pete = 0
+Niall = 0
 
 
 class donuts(commands.Cog):
@@ -26,19 +26,19 @@ class donuts(commands.Cog):
 
     @commands.command(pass_context = True)
     async def AddDonutVoss(self, ctx):
-        VossDonut = Voss + 1
+        Voss += 1
         await ctx.send('https://imgur.com/Z7T1Gh5')
 
     @commands.command(pass_context = True)
     async def DonutLeaderboard(self, ctx):
-        await ctx.send(Baldwin)
-        await ctx.send('Voss: ' + VossDonut)
-        await ctx.send(Ash)
-        await ctx.send(Jay)
-        await ctx.send(Aaron)
-        await ctx.send(Connor)
-        await ctx.send(Pete)
-        await ctx.send(Niall)
+        await ctx.send('Baldwin: ' + Baldwin)
+        await ctx.send('Voss: ' + Voss)
+        await ctx.send('Ash: ' + Ash)
+        await ctx.send('Jay: ' + Jay)
+        await ctx.send('Aaron: ' + Aaron)
+        await ctx.send('Connor: ' + Connor)
+        await ctx.send('Pete: ' + Pete)
+        await ctx.send('Niall: ' + Niall)
 
 
 
