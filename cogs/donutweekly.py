63aad7396116af82c1755ff317e4ca734d032cf6
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2020-08-05 22:41:14 +0100

    Add files via upload

--- cogs/donutweekly.py
+++ cogs/donutweekly.py
@@ -17,6 +17,9 @@ class donutweekly(commands.Cog):
 
     @tasks.loop(seconds=30)
     async def WeeklyReset(self):
+        channel = self.client.get_channel(523703758564360197)
+        await channel.send('test')
+        print('reset')
         global NiallWeek
         global VossWeek
         global ConnorWeek
@@ -33,9 +36,25 @@ class donutweekly(commands.Cog):
         AshWeek = 0
         PeteWeek = 0
         AaronWeek = 0
-        channel = self.client.get_channel(523703758564360197)
-        await channel.send('test')
 
+    @commands.command(pass_context = True)
+    async def Weekly(self, ctx):
+
+        embed = discord.Embed(
+            colour = discord.Colour.blue()
+        )
+
+        embed.set_author(name='Donut Leaderboard', icon_url='https://www.theflavorbender.com/wp-content/uploads/2014/09/Simpsons-Doughnuts-4238-Copy-1.jpg')
+        embed.add_field(name='Voss: ', value=('{}'.format(VossWeek)),inline=False)
+        embed.add_field(name='Ash: ', value=('{}'.format(AshWeek)),inline=False)
+        embed.add_field(name='Jay: ', value=('{}'.format(JayWeek)),inline=False)
+        embed.add_field(name='Baldwin: ', value=('{}'.format(BaldwinWeek)),inline=False)
+        embed.add_field(name='Pete: ', value=('{}'.format(AaronWeek)),inline=False)
+        embed.add_field(name='Niall: ', value=('{}'.format(NiallWeek)),inline=False)
+        embed.add_field(name='Aaron: ', value=('{}'.format(AaronWeek)),inline=False)
+        embed.add_field(name='Connor: ', value=('{}'.format(ConnorWeek)),inline=False)
+
+        await ctx.send(embed=embed)
 
 
 def setup(client):
