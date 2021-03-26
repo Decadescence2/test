78e93dec50b3876e5dbb014a0c4c060289061bec
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2021-03-26 02:27:44 +0000

    Add files via upload

--- cogs/Starboard.py
+++ cogs/Starboard.py
@@ -17,7 +17,7 @@ class Starboard(commands.Cog):
         print ('Starboard File Loaded')
 
     @commands.Cog.listener()
-    async def on_reaction_add(reaction, user):
+    async def on_reaction_add(reaction):
         if reaction.emoji == ':star:':
             await ctx.send('test')
 
