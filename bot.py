08436cc97c783325699924dd4de55f7ff40b3cbd
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2021-03-26 03:20:58 +0000

    Add files via upload

--- bot.py
+++ bot.py
@@ -80,8 +80,7 @@ async def on_message(message):
 
 @client.event
 async def on_reaction_add(reaction, user):
-    await client.process_commands(message)
-    await message.channel.send('test')
+    await bot.say('test')
 
 @client.command()
 async def Waffle(ctx):
