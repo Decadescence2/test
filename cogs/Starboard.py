23920348f51c6f83c2139c0c30ecdea290bd7334
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2021-03-26 04:11:17 +0000

    Add files via upload

--- cogs/Starboard.py
+++ cogs/Starboard.py
@@ -18,6 +18,7 @@ class Starboard(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
+        if payload.emoji.name == 'star'
         print('hello')
 
 
