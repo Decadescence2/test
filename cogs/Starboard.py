5273cd3469660c83ecaf131a5fae226947d32b0f
Decadescence2 <68042612+Decadescence2@users.noreply.github.com>
2021-03-26 02:24:16 +0000

    Add files via upload

---
+++ cogs/Starboard.py
@@ -0,0 +1,28 @@
+import discord
+import time
+import os
+import random
+from itertools import cycle
+from discord.ext import commands
+from random import randrange
+
+
+class Starboard(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print ('Starboard File Loaded')
+
+    @commands.Cog.listener()
+    async def on_reaction_add(reaction, user):
+        if reaction.emoji == ':star:':
+            await ctx.send('test')
+
+
+
+
+def setup(client):
+    client.add_cog(Starboard(client))
